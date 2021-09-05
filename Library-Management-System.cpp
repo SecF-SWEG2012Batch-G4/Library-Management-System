daeddd0be9c41be19f4b05fa0b8cabb02225fbf4
NatiShimelis <natishimelisg@gmail.com>
2021-09-05 18:55:20 +0300

    updated

--- Library-Management-System.cpp
+++ Library-Management-System.cpp
@@ -4,27 +4,33 @@ using namespace std;
 
 
 struct student{
-string sname, sid, bookId;
-};
+string f_name,l_name, sid, bookId;
+} stud[50] = {
+            {"tibebe","solomon","1234stud"},
+           {"tsion","tesfaye","1234stud"},
+           {"natnael","shimelis","1234stud"},
+           {"natnael","menelik","1234stud"}};
 
-struct Date{
+struct date{
 int dd,mm,yy;
 };
 
-struct book{
-string bId, bName, bAuthor;
-Date publishDate;
+struct bookinfo{
+int id;
+string  name, author;
+date dop;
 int status=0;
 string studid;
-};
+date return_date,issue_date;
+} book[50]={
+            {1,"Harry potter","J.K Rowlings",{12,23,2004}},
+            {2,"Fundamentals of Fluid Mechanics","Bruce Roy Munson",{30,9,2017}},
+            {3,"Introduction To Computing","Steven C Chapra",{02,12,2019}}};
 
 void issuebooks(void){
- string bookid, returnedbookid;
+    int bookid, returnedbookid;
     string sname;
-    student s1[50];
-    book b1[50];
     int counter = 0;
-   // b1[0]={}
     int choice;
     char option;
 
@@ -44,25 +50,26 @@ case 1:{
     cout<<"Enter book Id: ";
     cin>>bookid;
 for (int i=0; i<50; i++){
-    if (bookid == b1[i].bId){
-        cout<<"Name of the book: "<<b1[i].bName<<endl;
-        cout<<"Author of the book: "<<b1[i].bAuthor<<endl;
-        cout<<"ID of the book: "<<b1[i].bId<<endl;
-        cout<<"Date of Publish: "<<b1[i].publishDate.dd<<"/"<<b1[i].publishDate.mm<<"/"<<b1[i].publishDate.yy<<endl;
-Menu2:  cout<< "Do you want to borrow this book. Press 'Y' for Yes and 'N' for No ";
+    if (bookid == book[i].id){
+        cout<<"Name of the book: "<<book[i].name<<endl;
+        cout<<"Author of the book: "<<book[i].author<<endl;
+        cout<<"ID of the book: "<<book[i].id<<endl;
+        cout<<"Date of Publish: "<<book[i].dop.dd<<"/"<<book[i].dop.mm<<"/"<<book[i].dop.yy<<endl;
+Menu2:  cout<< "Do you want to borrow this book. Press 'Y' for Yes and 'N' for No "<<endl;
         cin>>option;
         if (option == 'Y'|| option == 'y'){
-            b1[i].status+=1;
+            book[i].status+=1;
 
-            cout<<"Enter your name: ";
-            cin>>s1[counter].sname;
             cout<<"Enter your ID: ";
-            cin>> s1[counter].sid;
-            s1[counter].bookId= bookid;
-            b1[counter].studid=s1[counter].sid;
+            cin>> stud[counter].sid;
+            stud[counter].bookId= bookid;
+            book[counter].studid=stud[counter].sid;
             counter++;
-            cout<<"When is the return date in(day,month,year): ";
-            cin>>b1[i].publishDate.dd>>b1[i].publishDate.mm>>b1[i].publishDate.yy;
+            cout<<"Enter the issue date in (day,month,year) format: ";
+            cin>>book[i].issue_date.dd>>book[i].issue_date.mm>>book[i].issue_date.yy;
+            cout<<"Enter the return date in(day,month,year) format: ";
+            cin>>book[i].return_date.dd>>book[i].return_date.mm>>book[i].return_date.yy;
+            cout<<endl;
         }
         else if(option == 'N'|| option == 'n')
             goto Menu;
@@ -71,19 +78,21 @@ Menu2:  cout<< "Do you want to borrow this book. Press 'Y' for Yes and 'N' for N
             goto Menu2;
         }
     }
-    else
+    else if( book[i].id == 0 ){
         cout<<"Sorry but this book ID does not exist"<<endl;
         goto Menu;
+        }
 }
     break;
     }
 case 2: {
         for (int i= 0; i<50; i++){
-            if (b1[i].status == 1){
+            if (book[i].status == 1){
                 cout<<"ID of borrower \t"<<"Book name \t"<< "Book author \t"<< "Return date"<<endl;
-                cout<<b1[i].studid<<"\t"<<b1[i].bName<<"\t"<<b1[i].bAuthor<<b1[i].publishDate.dd<<"/"<<b1[i].publishDate.mm<<"/"<<b1[i].publishDate.yy;
+                cout<<book[i].studid<<"\t"<<book[i].name<<"\t"<<book[i].author<<book[i].return_date.dd<<"/"<<book[i].return_date.mm<<"/"<<book[i].return_date.yy<<endl;
             }
         }
+        goto Menu;
         break;
 
 
@@ -92,13 +101,13 @@ case 3: {
         cout<<"Enter book ID: ";
         cin>>returnedbookid;
         for (int i= 0; i<50; i++){
-            if (returnedbookid == b1[i].bId){
-                b1[i].status=0;
+            if (returnedbookid == book[i].id){
+                book[i].status=0;
 }
         }
-
-        cout<<"Book updated";
-
+        cout<<"Book updated"<<endl<<endl;
+        goto Menu;
+    break;
     }
     }
 };
