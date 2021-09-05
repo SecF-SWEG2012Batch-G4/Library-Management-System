fbef05556e41d4d35b4e507d3149127648996473
NatiShimelis <natishimelisg@gmail.com>
2021-09-05 23:03:59 +0300

    Overdue payment with little update remaining

--- Library-Management-System.cpp
+++ Library-Management-System.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
-
-
+int timetaken;
+float payment;
 
 struct student{
 string f_name,l_name, sid, bookId;
@@ -21,7 +22,7 @@ string  name, author;
 date dop;
 int status=0;
 string studid;
-date return_date,issue_date;
+date return_date,issue_date,due_date;
 } book[50]={
             {1,"Harry potter","J.K Rowlings",{12,23,2004}},
             {2,"Fundamentals of Fluid Mechanics","Bruce Roy Munson",{30,9,2017}},
@@ -67,9 +68,10 @@ Menu2:  cout<< "Do you want to borrow this book. Press 'Y' for Yes and 'N' for N
             counter++;
             cout<<"Enter the issue date in (day,month,year) format: ";
             cin>>book[i].issue_date.dd>>book[i].issue_date.mm>>book[i].issue_date.yy;
-            cout<<"Enter the return date in(day,month,year) format: ";
-            cin>>book[i].return_date.dd>>book[i].return_date.mm>>book[i].return_date.yy;
+            cout<<"Enter the due date in(day,month,year) format: ";
+            cin>>book[i].due_date.dd>>book[i].due_date.mm>>book[i].due_date.yy;
             cout<<endl;
+            goto Menu;
         }
         else if(option == 'N'|| option == 'n')
             goto Menu;
@@ -89,7 +91,7 @@ case 2: {
         for (int i= 0; i<50; i++){
             if (book[i].status == 1){
                 cout<<"ID of borrower \t"<<"Book name \t"<< "Book author \t"<< "Return date"<<endl;
-                cout<<book[i].studid<<"\t"<<book[i].name<<"\t"<<book[i].author<<book[i].return_date.dd<<"/"<<book[i].return_date.mm<<"/"<<book[i].return_date.yy<<endl;
+                cout<<book[i].studid<<"\t"<<book[i].name<<"\t"<<book[i].author<<"\t"<<book[i].due_date.dd<<"/"<<book[i].due_date.mm<<"/"<<book[i].due_date.yy<<endl;
             }
         }
         goto Menu;
@@ -98,8 +100,54 @@ case 2: {
 
 }
 case 3: {
+    for (int i=0; i<50; i++){
         cout<<"Enter book ID: ";
         cin>>returnedbookid;
+        cout<<"Enter return date in (day/month/year) format: ";
+        cin>>book[i].return_date.dd>>book[i].return_date.mm>>book[i].return_date.yy;
+
+
+        //overdue payment checker
+        //Normal duration time limit for borrowed book= 5 days.
+
+        if (book[i].issue_date.mm == book[i].return_date.mm){
+            timetaken = (book[i].return_date.dd - book[i].issue_date.dd );
+overdue_calc:
+            if (timetaken<=5){
+                payment=0;
+            }
+            else if (timetaken>5 && timetaken<=10){ //5 birr per day
+                payment= (timetaken - 5) * 5;
+            }
+            else if (timetaken > 10 && timetaken<= 30){
+                payment = 5 * 5 + (timetaken - 10) * 10;
+            }
+            else{
+                payment = 5 * 5 + 20 * 10 + (timetaken - 30) * 20;
+            }
+
+            cout<< "Your overdue payment is: "<<payment<< " Birr."<<endl;
+
+        }
+        else if (book[i].issue_date.mm < book[i].return_date.mm){
+                if (book[i].return_date.mm == 3)
+                {
+                     timetaken= (book[i].return_date.dd + ( 28 - book[i].issue_date.dd ));
+                     goto overdue_calc;
+                }
+                else if (book[i].return_date.mm == 2 && book[i].return_date.mm == 4 && book[i].return_date.mm== 6 && book[i].return_date.mm== 8 &&
+                        book[i].return_date.mm== 9 && book[i].return_date.mm==11 )
+                {
+                    timetaken= (book[i].return_date.dd + ( 31 - book[i].issue_date.dd ));
+                    goto overdue_calc;
+                }
+                else
+                {
+                    timetaken= (book[i].return_date.dd + ( 30 - book[i].issue_date.dd ));
+                    goto overdue_calc;
+                }
+
+        }
         for (int i= 0; i<50; i++){
             if (returnedbookid == book[i].id){
                 book[i].status=0;
@@ -110,7 +158,9 @@ case 3: {
     break;
     }
     }
+    }
 };
+
 
 int main() {
     issuebooks();
