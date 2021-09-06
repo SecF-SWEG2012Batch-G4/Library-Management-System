ee3148dd61e6e10381fe6bae3746431478f51fef
NatiShimelis <natishimelisg@gmail.com>
2021-09-06 21:37:44 +0300

    Issue feature final

--- Library-Management-System.cpp
+++ Library-Management-System.cpp
@@ -1,32 +1,50 @@
 #include<iostream>
+#include<iomanip>
 #include<stdlib.h>
 using namespace std;
+
 int timetaken;
 float payment;
+int totalBooks= 7;
 
 struct student{
 string f_name,l_name, sid, bookId;
-} stud[50] = {
-            {"tibebe","solomon","1234stud"},
-           {"tsion","tesfaye","1234stud"},
-           {"natnael","shimelis","1234stud"},
-           {"natnael","menelik","1234stud"}};
+};
+student* stud  { new student[50] {
+            {"tibebe","solomon","0634"},
+           {"tsion","tesfaye","0645"},
+           {"natnael","shimelis","0524"},
+           {"natnael","menelik","0535"}}};
 
 struct date{
 int dd,mm,yy;
 };
 
-struct bookinfo{
-int id;
-string  name, author;
-date dop;
-int status=0;
-string studid;
-date return_date,issue_date,due_date;
-} book[50]={
-            {1,"Harry potter","J.K Rowlings",{12,23,2004}},
-            {2,"Fundamentals of Fluid Mechanics","Bruce Roy Munson",{30,9,2017}},
-            {3,"Introduction To Computing","Steven C Chapra",{02,12,2019}}};
+ struct bookinfo
+{
+   string name;
+   string category;//(category)
+   date dop;//Date of publication
+   string author;
+   int amount;//how many books there are
+   int id;//book id
+   float rating;//(out of 5)
+   int status;
+   //bool avail;//books availability
+   string studid;
+   date return_date,issue_date,due_date;
+};
+ bookinfo* book_info { new bookinfo[totalBooks] {
+            {"harry potter","Fiction",{12,23,2004},"J.K Rowlings",5,10,4.8, 0},
+            {"Fundamentals of Fluid Mechanics","Educational",{30,9,2017},"Bruce Roy Munson",3.9,12, 4.5, 0},
+            {"Introduction To Computing","Educational",{02,12,2019},"Steven C Chapra",6,2.3, 0},
+            {"Materials science ","Educational",{23,8,2021},"Steven C Chapra",2,6,5,0},
+            {"Perfect Chemistry","Romance",{28,02,2019},"Simone Elkeles",1,38631,4.8,0},
+            {"Dune","Romance",{05,17,2017},"Frank Herbert",9,125896,5,0},
+            {"Hamlet","Drama",{00,00,1601},"William Shakespeare",10,4582,4.6,0}
+			}};
+		//book* book_info = new book; //array of structures
+
 
 void issuebooks(void){
     int bookid, returnedbookid;
@@ -38,133 +56,158 @@ void issuebooks(void){
 Menu:
     cout<< "**************** Issue Menu ****************"<<endl;
     cout<<"**********************************************"<<endl;
-    cout<< "1. Issue book "<<endl;
-    cout<< "2. View issued books"<<endl;
-    cout<< "3. Remove issued book"<<endl;
-    cout<< "4. Exit"<<endl;
+    cout<< "1. List of available books "<<endl;
+    cout<< "2. Issue book "<<endl;
+    cout<< "3. View issued books"<<endl;
+    cout<< "4. Remove issued book"<<endl;
+    cout<< "5. Exit"<<endl;
     cout<<endl;
     cin>>choice;
 
     switch (choice){
 
 case 1:{
+	cout<<setw(10)<<"Book ID"<<setw(35)<<"Book Name"<<setw(35)<<"Book category"<<setw(25)<<"Rating\n";
+	cout<<"=====================================================================================================\n";
+for (int i= 0; i<totalBooks; i++){
+        if (book_info[i].status == 0)
+            {
+               cout<<setw(10)<<(book_info+i)->id<<setw(40) <<(book_info+i)->name<<setw(30)<< (book_info+i)->category<<setw(20)<< (book_info+i)->rating<<endl;
+            }
+
+        }
+        goto Menu;
+        break;
+    }
+case 2:{
     cout<<"Enter book Id: ";
     cin>>bookid;
-for (int i=0; i<50; i++){
-    if (bookid == book[i].id){
-        cout<<"Name of the book: "<<book[i].name<<endl;
-        cout<<"Author of the book: "<<book[i].author<<endl;
-        cout<<"ID of the book: "<<book[i].id<<endl;
-        cout<<"Date of Publish: "<<book[i].dop.dd<<"/"<<book[i].dop.mm<<"/"<<book[i].dop.yy<<endl;
-Menu2:  cout<< "Do you want to borrow this book. Press 'Y' for Yes and 'N' for No "<<endl;
+for (int i=0; i<totalBooks; i++){
+    if (bookid == book_info[i].id){
+        cout<<"Name of the book: "<<book_info[i].name<<endl;
+        cout<<"Author of the book: "<<book_info[i].author<<endl;
+        cout<<"ID of the book: "<<book_info[i].id<<endl;
+        cout<<"Date of Publish: "<<book_info[i].dop.dd<<"/"<<book_info[i].dop.mm<<"/"<<book_info[i].dop.yy<<endl<<endl;
+Menu2:  cout<< " \t Do you want to borrow this book. Press 'Y' for Yes and 'N' for No "<<endl;
         cin>>option;
         if (option == 'Y'|| option == 'y'){
-            book[i].status+=1;
-
+          book_info[i].status=1;
+			//book_info[i].avail= false;
             cout<<"Enter your ID: ";
             cin>> stud[counter].sid;
             stud[counter].bookId= bookid;
-            book[counter].studid=stud[counter].sid;
+            book_info[counter].studid=stud[counter].sid;
             counter++;
             cout<<"Enter the issue date in (day,month,year) format: ";
-            cin>>book[i].issue_date.dd>>book[i].issue_date.mm>>book[i].issue_date.yy;
-            cout<<"Enter the due date in(day,month,year) format: ";
-            cin>>book[i].due_date.dd>>book[i].due_date.mm>>book[i].due_date.yy;
+            cin>>book_info[i].issue_date.dd>>book_info[i].issue_date.mm>>book_info[i].issue_date.yy;
+            cout<<"The due date is: ";
+            book_info[i].due_date.dd= (book_info[i].issue_date.dd + 5 );
+            cout<<book_info[i].due_date.dd<<"/"<<book_info[i].issue_date.mm<<"/"<<book_info[i].issue_date.yy;
             cout<<endl;
+            cout<< " \t Book Issued Sucessfully! "<<endl<<endl;
             goto Menu;
         }
-        else if(option == 'N'|| option == 'n')
+       else if(option == 'N'|| option == 'n')
             goto Menu;
         else {
             cout<<"Wrong Input! Please try again.."<<endl;
             goto Menu2;
         }
     }
-    else if( book[i].id == 0 ){
+    else if (book_info[i].id == 0 ) {
         cout<<"Sorry but this book ID does not exist"<<endl;
         goto Menu;
         }
 }
+
     break;
     }
-case 2: {
-        for (int i= 0; i<50; i++){
-            if (book[i].status == 1){
-                cout<<"ID of borrower \t"<<"Book name \t"<< "Book author \t"<< "Return date"<<endl;
-                cout<<book[i].studid<<"\t"<<book[i].name<<"\t"<<book[i].author<<"\t"<<book[i].due_date.dd<<"/"<<book[i].due_date.mm<<"/"<<book[i].due_date.yy<<endl;
+
+case 3: {
+		 cout<<setw(10)<<"ID of borrower"<<setw(35)<<"Book name"<<setw(35)<< "Book author"<<setw(25)<< "Due date"<<endl;
+		 	cout<<"=====================================================================================================\n";
+        for (int i= 0; i<totalBooks; i++){
+        	if (book_info[i].status == 1)
+            {
+                cout<<setw(10)<< stud[i].sid <<setw(40)<< book_info[i].name<<setw(30)<< book_info[i].author<<setw(20)<<book_info[i].due_date.dd<<"/"<<book_info[i].issue_date.mm<<"/"<<book_info[i].issue_date.yy<<endl<<endl;
             }
+
         }
         goto Menu;
         break;
 
 
 }
-case 3: {
-    for (int i=0; i<50; i++){
+case 4: {
+	for (int i=0; i<totalBooks; i++){
         cout<<"Enter book ID: ";
         cin>>returnedbookid;
         cout<<"Enter return date in (day/month/year) format: ";
-        cin>>book[i].return_date.dd>>book[i].return_date.mm>>book[i].return_date.yy;
-
-
+        cin>>book_info[i].return_date.dd>>book_info[i].return_date.mm>>book_info[i].return_date.yy;
         //overdue payment checker
         //Normal duration time limit for borrowed book= 5 days.
 
-        if (book[i].issue_date.mm == book[i].return_date.mm){
-            timetaken = (book[i].return_date.dd - book[i].issue_date.dd );
+        if (book_info[i].issue_date.mm == book_info[i].return_date.mm)
+		{
+            timetaken = (book_info[i].return_date.dd - book_info[i].issue_date.dd );
 overdue_calc:
             if (timetaken<=5){
                 payment=0;
             }
             else if (timetaken>5 && timetaken<=10){ //5 birr per day
-                payment= (timetaken - 5) * 5;
+                payment= ((timetaken - 5) * 5);
             }
-            else if (timetaken > 10 && timetaken<= 30){
-                payment = 5 * 5 + (timetaken - 10) * 10;
-            }
-            else{
-                payment = 5 * 5 + 20 * 10 + (timetaken - 30) * 20;
+            else if (timetaken > 10 && timetaken<= 30){//15 birr per day
+                payment = (5 * 5 + (timetaken - 10) * 15 );
             }
 
             cout<< "Your overdue payment is: "<<payment<< " Birr."<<endl;
 
         }
-        else if (book[i].issue_date.mm < book[i].return_date.mm){
-                if (book[i].return_date.mm == 3)
+        else if (book_info[i].issue_date.mm  < book_info[i].return_date.mm){
+                if (book_info[i].return_date.mm == 3)
                 {
-                     timetaken= (book[i].return_date.dd + ( 28 - book[i].issue_date.dd ));
-                     goto overdue_calc;
+                     timetaken= (book_info[i].return_date.dd  + ( 28 - book_info[i].issue_date.dd ));
+    Notify:         if ( timetaken > 30){
+                	cout<< "You took more than 30 days to return this book."<<endl;
+                	cout<<"Your Membership to this library is canceled and your due payment is 300 birr!"<<endl;
+				}
                 }
-                else if (book[i].return_date.mm == 2 && book[i].return_date.mm == 4 && book[i].return_date.mm== 6 && book[i].return_date.mm== 8 &&
-                        book[i].return_date.mm== 9 && book[i].return_date.mm==11 )
+                else if (book_info[i].return_date.mm == 2 && book_info[i].return_date.mm == 4 && book_info[i].return_date.mm== 6 && book_info[i].return_date.mm== 8 &&
+                        book_info[i].return_date.mm== 9 && book_info[i].return_date.mm==11 )
                 {
-                    timetaken= (book[i].return_date.dd + ( 31 - book[i].issue_date.dd ));
-                    goto overdue_calc;
+                    timetaken= (book_info[i].return_date.dd  + ( 31 - book_info[i].issue_date.dd ));
+                    goto Notify;
                 }
                 else
                 {
-                    timetaken= (book[i].return_date.dd + ( 30 - book[i].issue_date.dd ));
-                    goto overdue_calc;
+                    timetaken= (book_info[i].return_date.dd + ( 30 - book_info[i].issue_date.dd ));
+                    goto Notify;
                 }
 
+
         }
-        for (int i= 0; i<50; i++){
-            if (returnedbookid == book[i].id){
-                book[i].status=0;
-}
-        }
-        cout<<"Book updated"<<endl<<endl;
-        goto Menu;
+
+        for (int i= 0; i<totalBooks; i++)
+		{
+            if (returnedbookid == book_info[i].id){
+               book_info[i].status=0;
+			}
+		}
+			cout<<" \t Book updated! "<<endl<<endl;
+        	goto Menu;
     break;
     }
     }
-    }
+}
 };
 
-
 int main() {
+
+
     issuebooks();
 
 
     return 0;
 }
+
